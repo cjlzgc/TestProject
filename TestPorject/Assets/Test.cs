2e2473b115d6d05d09f10fbd04811206831dfb45
Nick Zhao <cjlzgc@gmail.com>
2016-09-11 19:32:38 -0700

    add test files

---
+++ TestPorject/Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Test1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
